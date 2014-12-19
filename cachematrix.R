7dc136aa7291033212b067ab17923ba01ab3e048
Sean Stephenson <sms@pixar.com>
2014-12-18 21:35:57 -0800

    extend comments and scaffold out functions to look like example

--- cachematrix.R
+++ cachematrix.R
@@ -6,11 +6,21 @@
 ###############################################################################
 #
 # This function creates a special "matrix" object that can cache its inverse.
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of the inverse matrix
+# 4. get the value of the inverse matrix
 #
 ###############################################################################
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
 }
 
 
@@ -20,11 +30,13 @@ makeCacheMatrix <- function(x = matrix()) {
 #
 # This function computes the inverse of the special "matrix" returned by 
 # makeCacheMatrix above. If the inverse has already been calculated 
-# (and the matrix has not changed), then cacheSolve should  retrieve the 
+# (and the matrix has not changed), then cacheSolve should    retrieve the 
 # inverse from the cache.
 #
 ###############################################################################
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+
+    solve(makeCacheMatrix(x))
 }
