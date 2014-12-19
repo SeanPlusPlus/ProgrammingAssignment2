f161d172e26c8413c8c0044fed6a15cbf26b3fb8
Sean Stephenson <sms@pixar.com>
2014-12-18 21:47:38 -0800

    add statements similar to example ... still erring out thi

--- cachematrix.R
+++ cachematrix.R
@@ -42,6 +42,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-
-    solve(makeCacheMatrix(x))
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- inverse(data, ...)
+    x$setinverse(m)
+    m
 }
